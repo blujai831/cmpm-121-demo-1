30675b65d8ff27e6df87efc425b8ebe1983bdf6b
blujai831 <webmaster@blujai831.dev>
2024-10-08 06:28:15 -0700

    Add a currently nonfunctioning button element displaying a nonbinary person walking. This change will suffice to complete "Step 1" as prescribed by the school assignment for which purpose this fork is maintained.

--- src/main.ts
+++ src/main.ts
@@ -8,3 +8,8 @@ document.title = gameName;
 const header = document.createElement("h1");
 header.innerHTML = gameName;
 app.append(header);
+
+const theButton = document.createElement("button");
+theButton.innerHTML = "&#x1F6B6;";
+theButton.style.fontSize = '32pt';
+app.append(theButton);
