f411650aef79888d49eec26292e33ea46f1ea345
blujai831 <webmaster@blujai831.dev>
2024-10-12 15:14:43 -0700

    Replace the game with a better-coded and better-designed game.

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-This is the starter code project for the `Demo 1` project in CMPM 121, Game Development Patterns, at UC Santa Cruz (fall quarter, 2024). Students should incrementally modify this project, tracking their progress with git, to develop an incremental game of their own unique design.
+This fork is maintained as a deliverable for the `Demo 1` project in CMPM 121, Game Development Patterns, at UC Santa Cruz (fall quarter, 2024). I have incrementally modified this project, tracking my progress with git, to develop an incremental game of my own not-very-unique design.
+
+## Note to Adam
+
+I realize my implementation still comes in at closer to 200 than to 100 lines of code. I have followed to the best of my ability the detailed constructive criticism you were so kind as to take the time to offer me, and yet still I don't know how else to further optimize for low line count in my refactoring without doing something terrible like violating the 80-column rule or removing the empty lines between distinct code blocks, so I'm not sure how other students have been able to get their implementations down to around 100 lines of code assuming they have comparable feature sets. This notice is of course subject to change if I find a solution before the extended deadline.
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,164 @@
-import {GameState} from "./GameState.ts";
-import {GameUI} from "./GameUI.ts";
 import "./style.css";
 
-let ui = new GameUI();
-let state = new GameState();
-state.connectUI(ui);
-ui.present();
+const GAME_TITLE = "Feed the Fire";
+const GAME_PREMISE = `
+    You have discovered a new way to generate energy
+    by harvesting ecological damage done by global warming.
+    As a side effect, this reverses the damage.
+    Your miraculous discovery has made you the revered hero
+    of reasonable people and climate change denialists alike,
+    but you don't care about any of that. For you, the real question
+    is exactly what it's always been: how much more cheap junk
+    can you build and sell via this method?
+`;
+
+const MSEC_PER_SEC = 1000;
+const BASIC_ACTION_NAME = 'Fire';
+const BASIC_ACTION_DESCRIPTION = "&#x1F525; Makes things hotter.";
+const ITEM_COST_GROWTH_BASE = 1.15;
+const DECIMAL_PRECISION = 1;
+
+function arrayToRecord<K extends string | symbol | number, V, T>(
+    ts: readonly T[], k: (t: T) => K, v: (t: T) => V
+) {
+    return Object.fromEntries(ts.map(t => [k(t), v(t)])) as Record<K, V>;
+}
+
+interface Item {
+    name: ItemName,
+    cost: number,
+    rate: number,
+    description: string
+}
+
+const availableItems = [
+    {name: 'Cellphone', cost: 10, rate: 0.1, description: "&#x1F4F1; " +
+        "Hello? Hello? Can you hear me?"},
+    {name: 'Computer', cost: 100, rate: 2, description: "&#x1F4BB; " +
+        "No one knows what this is for other than playing video games."},
+    {name: 'Building', cost: 1000, rate: 50, description: "&#x1F3E2; " +
+        "Invented to contain homo sapiens infestations."},
+    {name: 'Rocket', cost: 10000, rate: 1500, description: "&#x1F680; " +
+        "Invented to cause homo sapiens infestations."},
+    {name: 'UFO', cost: 100000, rate: 52500, description: "&#x1F6F8; " +
+        "Invented to... hm... No, wait, this hasn't been invented yet."}
+] as const;
+
+type ItemName = typeof availableItems[number]['name'];
+
+function getItemRealCost(item: Item, alreadyHave: number): number {
+    return item.cost*(ITEM_COST_GROWTH_BASE**alreadyHave);
+}
+
+let gameState = {
+    globalWarmingIndex: 0,
+    globalWarmingRate: 0,
+    itemQuantities: arrayToRecord(availableItems, item => item.name, _ => 0)
+};
+
+function doBasicAction(state: typeof gameState) {
+    state.globalWarmingIndex += 1;
+}
+
+function canAffordItem(state: typeof gameState, item: Item): boolean {
+    return state.globalWarmingIndex >=
+        getItemRealCost(item, state.itemQuantities[item.name]);
+}
+
+function tryPurchaseItem(state: typeof gameState, item: Item): boolean {
+    let realCost = getItemRealCost(item, state.itemQuantities[item.name]);
+    if (state.globalWarmingIndex >= realCost) {
+        state.globalWarmingIndex -= realCost;
+        state.itemQuantities[item.name] += 1;
+        state.globalWarmingRate += item.rate;
+        return true;
+    } else return false;
+}
+
+function tickGameState(state: typeof gameState, interval: number) {
+    state.globalWarmingIndex += state.globalWarmingRate*interval/MSEC_PER_SEC;
+}
+
+document.title = GAME_TITLE;
+const app: HTMLDivElement = document.querySelector('#app')!;
+
+function makeElement(what: string, how?: (elem: HTMLElement) => void) {
+    const elem = document.createElement(what);
+    how?.call(elem, elem);
+    app.appendChild(elem);
+    return elem;
+}
+
+makeElement('h1', elem => elem.innerHTML = GAME_TITLE);
+const globalWarmingIndexDisplay =
+    makeElement('div', elem => elem.id = 'global-warming-index');
+makeElement('button', elem => {
+    elem.className = 'item-button';
+    elem.innerHTML =
+        `<strong>${BASIC_ACTION_NAME}</strong><br />${BASIC_ACTION_DESCRIPTION}`;
+    elem.onclick = _ => doBasicAction(gameState);
+});
+const itemButtons = arrayToRecord(availableItems, item => item.name, item =>
+    makeElement('button', elem => {
+        (elem as HTMLButtonElement).disabled = true;
+        elem.className = 'item-button not-unlocked';
+        elem.onclick = _ => tryPurchaseItem(gameState, item);
+    }) as HTMLButtonElement
+);
+const statusDisplay = makeElement('div');
+
+function updateUI(state: typeof gameState) {
+    if (state.globalWarmingIndex > 0) {
+        globalWarmingIndexDisplay.innerHTML =
+            `<p>${state.globalWarmingIndex.toFixed(DECIMAL_PRECISION)}
+                EcD units available</p>`;
+    } else {
+        globalWarmingIndexDisplay.innerHTML = `<p>${GAME_PREMISE}</p>`;
+    }
+    statusDisplay.innerHTML = "";
+    if (state.globalWarmingRate > 0) {
+        statusDisplay.innerHTML += `
+            <p>Passively generating
+            ${state.globalWarmingRate.toFixed(DECIMAL_PRECISION)}
+            units of ecological damage per second.</p>
+        `;
+    }
+    let listInnerHTML = "";
+    for (let item of availableItems) {
+        let qty = state.itemQuantities[item.name];
+        let button = itemButtons[item.name];
+        button.disabled = !canAffordItem(state, item);
+        if (!button.disabled) {
+            button.className = 'item-button unlocked';
+        }
+        button.innerHTML = `
+            <strong>${item.name}</strong><br />
+            Cost: ${getItemRealCost(item, qty).toFixed(DECIMAL_PRECISION)}
+                EcD units<br />
+            ${item.description}
+        `;
+        if (qty > 0) {
+            listInnerHTML +=
+                `<li>${qty.toFixed(0)}x ${item.name}</li>`;
+        }
+    }
+    if (listInnerHTML !== "") {
+        statusDisplay.innerHTML +=
+            `<p>Junk built:</p><ul>${listInnerHTML}</ul>`
+    }
+}
+
+function animateForever(
+    what: (interval: number) => void,
+    lastTimeStamp: DOMHighResTimeStamp = performance.now(),
+    thisTimeStamp: DOMHighResTimeStamp = performance.now()
+) {
+    what(thisTimeStamp - lastTimeStamp);
+    requestAnimationFrame(nextTimeStamp =>
+        animateForever(what, thisTimeStamp, nextTimeStamp));
+}
+
+animateForever(interval => {
+    tickGameState(gameState, interval);
+    updateUI(gameState);
+});
